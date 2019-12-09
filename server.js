8a59c354a8b9ccff09a71471474ac2890407d4b7
noi22 <yy.inoi@gmail.com>
2019-12-09 10:59:08 +0900

    add option

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 const options = {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
     user: process.env.DB_USER,
     pass: process.env.DB_PASSWORD,
 }
